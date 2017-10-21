306f0c947dc59e21819709bb5b180fcd7ee9f8bd
Edward D'Amato <info@edwarddamato.com>
2017-10-21 22:44:28 +0100

    Change App component to a class

--- src/ui/components/App/App.jsx
+++ src/ui/components/App/App.jsx
@@ -4,16 +4,28 @@ import Footer from '../Footer';
 import WelcomeScreen from '../WelcomeScreen';
 import './App.scss';
 
-const App = () => (
-  <div className="root_container">
-    <Header />
-    {/* We're using Node.js {process.versions.node},
-    Chromium {process.versions.chrome},
-    and Electron {process.versions.electron}. */}
+class App extends React.Component {
+  constructor (props) {
+    super(props);
 
-    <WelcomeScreen />
-    <Footer />
-  </div>
-);
+    this.state = {
+      loading: false
+    };
+  }
+
+  render () {
+    return (
+      <div className="root_container">
+        <Header />
+        {/* We're using Node.js {process.versions.node},
+        Chromium {process.versions.chrome},
+        and Electron {process.versions.electron}. */}
+
+        <WelcomeScreen />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;
