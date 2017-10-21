dd426f426fc9c5f3a46a2208a88fc08307b5546b
Edward D'Amato <info@edwarddamato.com>
2017-10-21 22:42:01 +0100

    Create Loader component

---
+++ src/ui/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () => (
+  <section className="loader">
+    <div className="loader_block loader_block--1"></div>
+    <div className="loader_block loader_block--2"></div>
+    <div className="loader_block loader_block--3"></div>
+    <div className="loader_block loader_block--4"></div>
+  </section>
+);
+
+export default Loader;
